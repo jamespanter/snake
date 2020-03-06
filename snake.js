a200972cc5737c53a07a5a17c660cedb9bdef374
James Panter <James.Panter@Nology.local>
2020-03-06 13:53:07 +0000

    snake tail no longer stays

--- snake.js
+++ snake.js
@@ -1,17 +1,17 @@
-const canvas = document.getElementById("snake-container");
-const ctx = canvas.getContext("2d");
-let snakeX = 100;
-let snakeY = 200;
-let dx = 0.5;
-let dy = 0.5;
-let direction = 39;
+const canvas = document.getElementById("snake-container"),
+  ctx = canvas.getContext("2d");
+let snakeX = 100,
+  snakeY = 200;
+const arrayOfSnakeXCoords = [snakeX];
+const arrayOfNodeYCoords = [snakeY];
+let dx = 1;
+let dy = 1;
+let direction;
 
 const newGame = () => {
-  snakeX = 100;
-  snakeY = 200;
-  dx = 0.5;
-  dy = 0.5;
-
+  snakeX = 200;
+  snakeY = 250;
+  direction = 0;
   clearCanvas();
   createNode();
   setInterval(moveSnake, 30);
@@ -22,35 +22,55 @@ const clearCanvas = () => {
 };
 
 const createNode = () => {
-  const x = Math.floor(Math.random() * canvas.width);
-  const y = Math.floor(Math.random() * canvas.height);
+  let x = Math.floor(Math.random() * canvas.width),
+    y = Math.floor(Math.random() * canvas.height);
+  if (x > 780) {
+    x = 780;
+  }
+  if (y > 480) {
+    y = 480;
+  }
   createNodeOnCanvas(x, y);
 };
 
 const createNodeOnCanvas = (x, y) => {
+  //   arrayOfSnakeXCoords.push(x);
+  //   arrayOfNodeYCoords.push(y);
   ctx.fillStyle = "black";
   ctx.fillRect(x, y, 20, 20);
 };
 
 const moveSnake = () => {
-  let moveDirection;
+  console.log(arrayOfSnakeXCoords, arrayOfNodeYCoords);
+  //clear end of snake
+  ctx.clearRect(arrayOfSnakeXCoords[0], arrayOfNodeYCoords[0], 20, 20);
+  //remove end of snake from arrays
+  arrayOfNodeYCoords.splice(0, 1);
+  arrayOfSnakeXCoords.splice(0, 1);
   ctx.fillStyle = "red";
-  ctx.clearRect(snakeX, snakeY, 20, 20);
+  //create head of snake
   ctx.fillRect(snakeX, snakeY, 20, 20);
-  if (direction == 38) {
-    moveDirection = snakeY;
-    snakeY -= dy;
-  } else if (direction == 40) {
-    moveDirection = snakeY;
-    snakeY += dy;
-  } else if (direction == 37) {
-    moveDirection = snakeX;
-    snakeX -= dx;
-  } else if (direction == 39) {
-    moveDirection = snakeX;
-    snakeX += dx;
+  //add head of snake to array
+  arrayOfSnakeXCoords.push(snakeX);
+  arrayOfNodeYCoords.push(snakeY);
+  switch (direction) {
+    // left
+    case 37:
+      snakeX -= dx;
+      break;
+    // up
+    case 38:
+      snakeY -= dy;
+      break;
+    // right
+    case 39:
+      snakeX += dx;
+      break;
+    // down
+    case 40:
+      snakeY += dy;
+      break;
   }
-
   //   console.log("snakeX = " + snakeX, "snakeY = " + snakeY);
 };
 
@@ -58,18 +78,4 @@ newGame();
 
 document.getElementById("new-game-button").addEventListener("click", newGame);
 document.getElementById("create-node").addEventListener("click", createNode);
-window.addEventListener("keydown", e => {
-  if (e.keyCode == "38") {
-    direction = 38;
-    // up arrow
-  } else if (e.keyCode == "40") {
-    direction = 40;
-    // down arrow
-  } else if (e.keyCode == "37") {
-    direction = 37;
-    // left arrow
-  } else if (e.keyCode == "39") {
-    direction = 39;
-    // right arrow
-  }
-});
+window.addEventListener("keydown", e => (direction = e.keyCode));
